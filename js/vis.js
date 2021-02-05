c7e3bb3f4a20a1149becdbd6be1a46fe10e0e65b
LEE Edwin <Edwin.LEE@murex.com>
2021-02-05 14:35:37 +0800

    test

--- js/vis.js
+++ js/vis.js
@@ -1,7 +1,7 @@
 
 var urlParams = new URLSearchParams(window.location.search);
 
-graphFileUrl = 'https://edwinlee44.github.io/test/'
+graphFileUrl = 'https://stash.murex.com/rest/api/1.0/users/elee/repos/fubon_upgrade/browse/graphs'
 ondemand = urlParams.get('ondemand')
 
 
